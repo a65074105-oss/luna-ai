5912873250ba01fcc6e71374f6089fb425246087
a65074105-oss <a65074105@gmail.com>
2026-07-10 23:24:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Luna AI запущен");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card, index) => {
+
+        card.style.opacity = "0";
+        card.style.transform = "translateY(40px)";
+
+        setTimeout(() => {
+
+            card.style.transition = "0.8s ease";
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+
+        }, 300 + index * 250);
+
+    });
+
+});
